cb68d8cdc6e04a95ab09233f7356c5a91910c681
Tommaso <tommaso.brini@studio.unibo.it>
2025-04-30 12:16:06 +0200

    Solved step1

--- pps-lab01/tdd-exercise/src/main/java/tdd/model/SmartDoorLockImpl.java
+++ pps-lab01/tdd-exercise/src/main/java/tdd/model/SmartDoorLockImpl.java
@@ -4,51 +4,95 @@ import tdd.SmartDoorLock;
 
 public class SmartDoorLockImpl implements SmartDoorLock {
 
+    private enum Status{
+        LOCKED, UNLOCKED, BLOCKED
+    }
+
     private static final int DEFAULT_PIN = 0;
+    private static final int PIN_LENGTH = 4;
+    private static final int INITIAL_ATTEMPTS = 0;
+    private static final int MAX_ATTEMPTS = 3;
 
     private int pin;
+    private Status status;
+    private int attempts;
 
     public SmartDoorLockImpl(){
-        this.pin = DEFAULT_PIN;
+        this.reset();
     }
 
     @Override
     public void setPin(int pin) {
-
+        if(!this.isLocked() && !this.isBlocked()){
+            if(this.checkPin(pin)){
+                throw new IllegalArgumentException("Pin must be 4-digit number");
+            }
+            this.pin = pin;
+        } else {
+            throw new IllegalStateException("Door is locked or blocked");
+        }
     }
+
+    private boolean checkPin(int pin){
+        return String.valueOf(pin).length() != PIN_LENGTH;
+    }
+
 
     @Override
     public void unlock(int pin) {
+        if(!this.isBlocked()){
+            if (this.pin == pin){
+                this.attempts = INITIAL_ATTEMPTS;
+                this.status = Status.UNLOCKED;
+            } else {
+                this.fail();
+            }
+        }
+    }
 
+    private void fail(){
+        this.attempts += 1;
+        if(this.attempts > MAX_ATTEMPTS){
+            this.status = Status.BLOCKED;
+        }
     }
 
     @Override
     public void lock() {
+        if(!this.isBlocked()){
+            if(pin != DEFAULT_PIN){
+                this.status = Status.LOCKED;
+            } else {
+                throw new IllegalStateException("Pin is not set");
+            }
+        }
 
     }
 
     @Override
     public boolean isLocked() {
-        return false;
+        return this.status == Status.LOCKED;
     }
 
     @Override
     public boolean isBlocked() {
-        return false;
+        return this.status == Status.BLOCKED;
     }
 
     @Override
     public int getMaxAttempts() {
-        return 0;
+        return MAX_ATTEMPTS;
     }
 
     @Override
     public int getFailedAttempts() {
-        return 0;
+        return this.attempts;
     }
 
     @Override
     public void reset() {
-
+        this.attempts = INITIAL_ATTEMPTS;
+        this.status = Status.UNLOCKED;
+        this.pin = DEFAULT_PIN;
     }
 }
--- pps-lab01/tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
+++ pps-lab01/tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
@@ -9,6 +9,9 @@ import static org.junit.jupiter.api.Assertions.*;
 public class SmartDoorLockTest {
 
     private static final int PIN = 1234;
+    private static final int WRONG_PIN = 4321;
+    private static final int WRONG_ATTEMPTS = 4;
+    private static final int RESET_ATTEMPTS = 0;
 
     private SmartDoorLock smartDoorLock;
 
@@ -16,6 +19,55 @@ public class SmartDoorLockTest {
     public void setup(){
         this.smartDoorLock = new SmartDoorLockImpl();
         this.smartDoorLock.setPin(PIN);
+    }
+
+    @Test
+    public void testErrorSetPin(){
+        assertThrows(IllegalArgumentException.class, () -> this.smartDoorLock.setPin(0));
+    }
+
+    @Test
+    public void testWrongStatus(){
+        this.smartDoorLock.lock();
+        assertThrows(IllegalStateException.class, () -> this.smartDoorLock.setPin(PIN));
+    }
+
+    @Test
+    public void testLock(){
+        this.smartDoorLock.lock();
+        assertTrue(this.smartDoorLock.isLocked());
+    }
+
+    @Test
+    public void testUnlock(){
+        this.smartDoorLock.unlock(PIN);
+        assertFalse(this.smartDoorLock.isLocked());
+    }
+
+    @Test
+    public void testBlockedState(){
+        this.blockDoor();
+        assertTrue(this.smartDoorLock.isBlocked());
+    }
+
+    @Test
+    public void testBlockedBehaviour(){
+        this.blockDoor();
+        this.smartDoorLock.lock();
+        assertFalse(smartDoorLock.isLocked());
+    }
+
+    private void blockDoor(){
+        for (int i = 0; i < WRONG_ATTEMPTS; i++) {
+            this.smartDoorLock.unlock(WRONG_PIN);
+        }
+    }
+
+    @Test
+    public void testResetStatus(){
+        this.blockDoor();
+        this.smartDoorLock.reset();
+        assertEquals(this.smartDoorLock.getFailedAttempts(), RESET_ATTEMPTS);
     }
 
 }
