b3e6ff9817928b6e219a6e2cdd3c5f01e5315202
Tommaso <tommaso.brini@studio.unibo.it>
2025-05-05 17:26:46 +0200

    Load lab01b

---
+++ pps-lab01b/src/test/java/e2/LogicTest.java
@@ -0,0 +1,15 @@
+package e2;
+import org.junit.jupiter.api.*;
+
+public class LogicTest {
+
+  @Test
+  public void test() {
+    assert(true);
+    // TODO: Add your test logic here
+    // You can generate random inputs and assert the expected output
+    // For example:
+    // int result = Logic.someMethod(5, 10);
+    // assertEquals(expectedResult, result);
+  }
+}
